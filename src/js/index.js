f058339da6b28686a57fc2f5a4af13100b0a2a3a
Darmaabazar <bazaracer4@gmail.com>
2024-05-03 20:27:48 +0800

    thinking page done

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import LocomotiveScroll from 'locomotive-scroll';
 import '../css/style.css';
 import '../css/locomotive-scroll.css';
+import '../css/services.css';
+import '../css/careers.css';
 
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
